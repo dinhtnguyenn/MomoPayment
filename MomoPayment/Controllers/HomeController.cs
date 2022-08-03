73aac2e15158833d16e4ac347f4fbeb40ce78ce2
Dinh Nguyen <nguyendinhteki@gmail.com>
2022-08-03 21:28:03 +0700

    update

--- MomoPayment/Controllers/HomeController.cs
+++ MomoPayment/Controllers/HomeController.cs
@@ -25,10 +25,10 @@ namespace MomoPayment.Controllers
             string serectkey = "sFcbSGRSJjwGxwhhcEktCHWYUuTuPNDB";
             string orderInfo = "test";
             string returnUrl = "https://localhost:44394/Home/ConfirmPaymentClient";
-            string notifyurl = "http://ba1adf48beba.ngrok.io/Home/SavePayment"; //lưu ý: notifyurl không được sử dụng localhost, có thể sử dụng ngrok để public localhost trong quá trình test
+            string notifyurl = "https://4c8d-2001-ee0-5045-50-58c1-b2ec-3123-740d.ap.ngrok.io/Home/SavePayment"; //lưu ý: notifyurl không được sử dụng localhost, có thể sử dụng ngrok để public localhost trong quá trình test
 
             string amount = "1000";
-            string orderid = DateTime.Now.Ticks.ToString();
+            string orderid = DateTime.Now.Ticks.ToString(); //mã đơn hàng
             string requestId = DateTime.Now.Ticks.ToString();
             string extraData = "";
 
@@ -75,16 +75,20 @@ namespace MomoPayment.Controllers
 //Khi thanh toán xong ở cổng thanh toán Momo, Momo sẽ trả về một số thông tin, trong đó có errorCode để check thông tin thanh toán
 //errorCode = 0 : thanh toán thành công (Request.QueryString["errorCode"])
 //Tham khảo bảng mã lỗi tại: https://developers.momo.vn/#/docs/aio/?id=b%e1%ba%a3ng-m%c3%a3-l%e1%bb%97i
-        public ActionResult ConfirmPaymentClient()
+        public ActionResult ConfirmPaymentClient(Result result)
         {
-            //hiển thị thông báo cho người dùng
+            //lấy kết quả Momo trả về và hiển thị thông báo cho người dùng (có thể lấy dữ liệu ở đây cập nhật xuống db)
+            string rMessage  = result.message;
+            string rOrderId = result.orderId;
+            string rErrorCode = result.errorCode; // = 0: thanh toán thành công
             return View();
         }
-        
+
         [HttpPost]
         public void SavePayment()
         {
             //cập nhật dữ liệu vào db
+            String a = "";
         }
     }
 }
---
+++ MomoPayment/Models/Result.cs
@@ -0,0 +1,18 @@
+﻿public class Result
+{
+    public string partnerCode { get; set; }
+    public string accessKey { get; set; }
+    public string requestId { get; set; }
+    public string amount { get; set; }
+    public string orderId { get; set; }
+    public string orderInfo { get; set; }
+    public string orderType { get; set; }
+    public string transId { get; set; }
+    public string message { get; set; }
+    public string localMessage { get; set; }
+    public string responseTime { get; set; }
+    public string errorCode { get; set; }
+    public string payType { get; set; }
+    public string extraData { get; set; }
+    public string signature { get; set; }
+}
